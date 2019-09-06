e87ffb5c5eed789ebd1b70b97517a0a66773c1b9
ZeydT <36661453+ZeydT@users.noreply.github.com>
2019-09-06 13:02:03 +0200

    Add files via upload

--- Pythonnn.py
+++ Pythonnn.py
@@ -6,6 +6,29 @@ for i in range(10):
     groet()
 """
 
+"""
 def tafel5():
     print("5, 10, 15, 20, 25, 30, 35, 40, 45, 50")
 tafel5()
+"""
+
+"""
+bedrijven = {"Apple": "Steve Jobs", "Microsoft": "Bill Gates"}
+
+print(bedrijven["Apple"])
+print(bedrijven["Microsoft"])
+"""
+
+"""
+def groet(naam):
+    print("Hallo " + naam)
+  
+groet("Kees")
+groet("Loes")
+groet("Anne")
+"""
+
+"""
+ingrediënten = {1: "boter", 2: "kaas", 3:, "eieren"}
+del ingrediënten["3"]
+"""
